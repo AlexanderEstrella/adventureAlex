fdecbd994226805c9102461c32ca9a7c5d075c9d
AlexanderEstrella <alexanderestrella23@gmail.com>
2023-07-05 23:48:58 -0400

    hosting

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
-import Navbar from "./Components/Navbar";
-import About from "./Pages/About.";
+import About from "./Pages/About";
 import Footer from "./Components/Footer";
 
 function App() {
