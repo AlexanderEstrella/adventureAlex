30fd83a2fa74f41ee8cc083c09e0a4096de9edc1
Alexanderestrella <alexanderestrella23@gmail.com>
2024-03-13 16:51:51 -0400

    created user friendly nav bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,40 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faPerson,
-  faHouse,
-  faEarthAmericas,
   faBars
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [mobile, setMobile] = useState(false);
+
+  const toggleScreen = () => {
+    setMobile(!mobile);
+  }
+
   return (
     <div className="primaryHeader">
       <div className="Headertitle">
         <h1 className="Logo">Adventurer Alex's</h1>
       </div>
       <div className="nav">
-        <ul>
+        <ul className="navLinks">
           <li>
-            <Link classname="navlinks" to="/Home">
+            <Link to="/Home">
               <h3>Home</h3>
             </Link>
           </li>
           <li>
             <Link to="/About">
-            <h3>About Us</h3>
+              <h3>About Us</h3>
             </Link>
           </li>
           <li>
-            <Link classname="navlinks" to="/Adventures">
-            <h3>Adventures</h3>
+            <Link to="/Adventures">
+              <h3>Adventures</h3>
             </Link>
           </li>
         </ul>
-      </div>
-      <div className="hamburger-icon">
-        <FontAwesomeIcon icon={faBars} />
+        <span className="FaBars" onClick={toggleScreen}>
+          <FontAwesomeIcon icon={faBars} />
+        </span>
+        <ul className="Dropdownmenu" style={{ display: mobile ? "block" : "none" }}>
+          <li>
+            <Link to="/Home">
+              <h3>Home</h3>
+            </Link>
+          </li>
+          <li>
+            <Link to="/About">
+              <h3>About Us</h3>
+            </Link>
+          </li>
+          <li>
+            <Link to="/Adventures">
+              <h3>Adventures</h3>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
